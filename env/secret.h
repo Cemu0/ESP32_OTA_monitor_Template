acc2f000ee0eb7a09ed73689a21aaa0023171d33
quoc.anh.do.nguyen <anh.nguyen@team.eyeq.tech>
2025-10-18 11:05:25 +0700

    modify secret

--- env/secret.h
+++ env/secret.h
@@ -1,5 +1,5 @@
 #define SECRET_SSID1 "YourWifi2p4GHZName"
 #define SECRET_PASS1 "YourPASS"
 
-#define SSID2 "YourSSID"
-#define PASS2 "YourPASS"
+#define SECRET_SSID2 "YourSSID"
+#define SECRET_PASS2 "YourPASS"
